30edc8765f3e285e4215e83fd7acb1dd1f029631
Faiver <ferdouskhalifa77@gmail.com>
2025-07-29 19:03:14 +0600

    Add files via upload

---
+++ vendor/prefixed/mpdf/src/Tag/Del.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @license GPL-2.0-only
+ *
+ * Modified by kodezen on 22-July-2025 using {@see https://github.com/BrianHenryIE/strauss}.
+ */
+
+namespace StoreEngine\Mpdf\Tag;
+
+class Del extends InlineTag
+{
+
+
+}
