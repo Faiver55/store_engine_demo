3b34131b7bd9c31f326079fa1fc85ee2a9358509
Faiver <ferdouskhalifa77@gmail.com>
2025-07-29 18:58:08 +0600

    Add files via upload

---
+++ vendor/prefixed/psr/log/Psr/Log/InvalidArgumentException.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @license MIT
+ *
+ * Modified by kodezen on 22-July-2025 using {@see https://github.com/BrianHenryIE/strauss}.
+ */
+
+namespace StoreEngine\Psr\Log;
+
+class InvalidArgumentException extends \InvalidArgumentException
+{
+}
